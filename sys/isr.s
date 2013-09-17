6d4b19892e1f75df3280a018df34af8292c578c1
Dushyant Goyal <goyal1dushyant@gmail.com>
2013-09-17 02:01:01 -0400

    playing with isr.s

--- sys/isr.s
+++ sys/isr.s
@@ -6,7 +6,7 @@ _isr_000:
   pushq 0x00      # Push the interrupt number (0)
   jmp isr_common_stub  # Go to our common handler. 
   sti
-  retq
+  iretq
 
 isr_common_stub:
   pushq %rax
@@ -17,7 +17,27 @@ isr_common_stub:
   pushq %rsi
   pushq %rbp
   pushq %rsp           # Pushes the general purpose registers GPR's R8-R15 also might come
+
+#trying to restore the code segment, instruction pointer, flags register, stack segment and stack pointer                         
+  
+  movq %rax, %ds         # Lower 16-bits of eax = ds.
+  pushq %rax            # save the data segment descriptor
+  movq %rax, 0x10       # load the kernel data segment descriptor
+  movq %ds, %rax
+  movq %es, %rax
+  movq %fs, %rax
+  movq %gs, %rax
+  movq %ss, %rax
+  
   call isr_handler_0
+  
+  popq %rax        # reload the original data segment descriptor
+  movq %ds, %rax
+  movq %es, %rax
+  movq %fs, %rax
+  movq %gs, %rax
+  movq %ss, %rax
+  
   popq %rsp
   popq %rbp
   popq %rsi
@@ -25,6 +45,9 @@ isr_common_stub:
   popq %rdx
   popq %rcx
   popq %rbx
-  popq %rax            # Pops the general purpose registers GPR's R8-R15 also might come
+  popq %rax        # Pops the general purpose registers GPR's R8-R15 also might come
+  
+  addq %rsp, 8     # Cleans up the pushed error code and pushed ISR number
+  
   iretq           
 
