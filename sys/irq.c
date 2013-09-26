bf84690b63685cf6414442b4d7f7ec8f8b5f69f8
Dushyant Goyal <goyal1dushyant@gmail.com>
2013-09-26 03:45:33 -0400

    fixing few keyboard keys

--- sys/irq.c
+++ sys/irq.c
@@ -72,6 +72,51 @@ unsigned char keyboard_map[128] =
  
 };		
 
+unsigned char keyboard_map_shift[128] =
+{
+    0,  27, 
+    '!', '@', '#', '$', '%', '^', '&', '*',	/* 9 */
+  '(', ')', '_', '+',
+    '\b',	/* Backspace */
+  '\t',			/* Tab */
+  'Q', 'W', 'E', 'R',	/* 19 */
+  'T', 'Y', 'U', 'I', 'O', 'P', '{', '}', '\n',	/* Enter key */
+    0,			/* 29   - Control */
+  'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', ':',	/* 39 */
+ '\"', '~',   0,		/* Left shift */
+ '|', 'Z', 'X', 'C', 'V', 'B', 'N',			/* 49 */
+  'M', '<', '>', '?',   0,				/* Right shift */
+  '*',
+    0,	/* Alt */
+  ' ',	/* Space bar */
+    0,	/* Caps lock */
+    0,	/* 59 - F1 key ... > */
+    0,   0,   0,   0,   0,   0,   0,   0, // 67
+    0,	/* < ... F10 */
+    0,	/* 69 - Num lock*/
+    0,	/* Scroll Lock */
+    0,	/* Home key */
+    0,	/* Up Arrow */
+    0,	/* Page Up */
+  '-',
+    0,	/* Left Arrow */
+    0,
+    0,	/* Right Arrow */
+  '+',
+    0,	/* 79 - End key*/
+    0,	/* Down Arrow */
+    0,	/* Page Down */
+    0,	/* Insert Key */
+    0,	/* Delete Key */
+    0,   0,   0,
+    0,	/* F11 Key */
+    0,	/* F12 Key */
+    0,	/* All other keys are undefined */ //88
+ 
+};		
+
+
+
 void PIC_sendEOI(unsigned char irq)
 {
   if(irq >= 8)
@@ -168,12 +213,13 @@ void irq_handler_1(registers_t regs)
     {
       if(shift_pressed)
       {
-        if( keyboard_map[scancode] >=97 && keyboard_map[scancode] <=122 )
+        printtoside(keyboard_map_shift[scancode]);
+        /*if( keyboard_map[scancode] >=97 && keyboard_map[scancode] <=122 )
             printtoside(keyboard_map[scancode]-32);
 //          putchar(keyboard_map[scancode]-32);
         if( scancode >=2 && scancode <= 13)
             printtoside(keyboard_map[scancode+88]);
-//          putchar(keyboard_map[scancode+88]);
+//          putchar(keyboard_map[scancode+88]);*/
         shift_pressed=0;
       } 
       else
