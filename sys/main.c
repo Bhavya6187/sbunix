dd952b4219963d3875154178e4dd9b921b4384c5
Dushyant Goyal <goyal1dushyant@gmail.com>
2013-10-11 01:32:32 -0400

    Ferdman's new code

--- sys/main.c
+++ sys/main.c
@@ -5,18 +5,22 @@
 #include <sys/isr.h>
 #include <sys/irq.h>
 
-void start(void* modulep, void* physbase, void* physfree)
+void start(uint32_t* modulep, void* physbase, void* physfree)
 {
- // int a, b,c;
- // a=5;
- // b=0;
- // c = 9;
+	struct smap_t {
+		uint64_t base, length;
+		uint32_t type;
+	}__attribute__((packed)) *smap;
+	printf("physbase-%x\n physfree %x\n", (uint64_t)physbase, (uint64_t)physfree);
+	//printf("modeulep = %x %x %x\n", (uint64_t)modulep[0], (uint64_t)modulep[1], (uint64_t)&modulep);
+	while(modulep[0] != 0x9001) modulep += modulep[1]+2;
+	for(smap = (struct smap_t*)(modulep+2); smap < (struct smap_t*)((char*)modulep+modulep[1]+2*4); ++smap) {
+		if (smap->type == 1 /* memory */ && smap->length != 0) {
+			printf("Available Physical Memory [%x-%x]\n", smap->base, smap->base + smap->length);
+		}
+	}
 	// kernel starts here
-//  printf("Checking printf before ISR()\n");
-//  c= a/b;
-//  printf("Checking printf after ISR() %d %d %d\n", a,b,c);
-//  printf("Checking printf\n");
-  printf("Project has been made by Bhavya Agarwal and Dushyant Goyal\n");
+	while(1);
 }
 
 #define INITIAL_STACK_SIZE 4096
@@ -27,25 +31,25 @@ extern char kernmem, physbase;
 void boot(void)
 {
 	// note: function changes rsp, local stack variables can't be practically used
-	//register char *temp1, *temp2;
+	register char *temp1, *temp2;
 	__asm__(
 		"movq %%rsp, %0;"
 		"movq %1, %%rsp;"
 		:"=g"(loader_stack)
 		:"r"(&stack[INITIAL_STACK_SIZE])
 	);
-  clrscr();
 	reload_gdt();
+	setup_tss();
 	reload_idt();
 	start(
-		(char*)(uint64_t)loader_stack[3] + (uint64_t)&kernmem - (uint64_t)&physbase,
+		(uint32_t*)((char*)(uint64_t)loader_stack[3] + (uint64_t)&kernmem - (uint64_t)&physbase),
 		&physbase,
 		(void*)(uint64_t)loader_stack[4]
 	);
-	/*for(
+	for(
 		temp1 = "!!!!! start() returned !!!!!", temp2 = (char*)0xb8000;
 		*temp1;
 		temp1 += 1, temp2 += 2
-	) *temp2 = *temp1;*/
+	) *temp2 = *temp1;
 	while(1);
 }
