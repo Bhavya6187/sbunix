f812f2961afb4ad939b07b61338733663535467a
bhavya <bhavya.6187@gmail.com>
2013-11-11 22:06:28 -0500

    fixed errors

--- sys/main.c
+++ sys/main.c
@@ -1,9 +1,25 @@
 #include <defs.h>
+#include <stdio.h>
 #include <sys/gdt.h>
+#include <sys/idt.h>
+#include <sys/isr.h>
+#include <sys/irq.h>
+#include <sys/phy_mem.h>
+#include <sys/page_table.h>
+#include <sys/tasking.h>
 #include <sys/tarfs.h>
+
+
+
+#define INITIAL_STACK_SIZE 4096
+char stack[INITIAL_STACK_SIZE];
+uint32_t* loader_stack;
+extern char kernmem, physbase;
+struct tss_t tss;
 
 void start(uint32_t* modulep, void* physbase, void* physfree)
 {
+  clrscr();
 	struct smap_t {
 		uint64_t base, length;
 		uint32_t type;
@@ -12,18 +28,18 @@ void start(uint32_t* modulep, void* physbase, void* physfree)
 	for(smap = (struct smap_t*)(modulep+2); smap < (struct smap_t*)((char*)modulep+modulep[1]+2*4); ++smap) {
 		if (smap->type == 1 /* memory */ && smap->length != 0) {
 			printf("Available Physical Memory [%x-%x]\n", smap->base, smap->base + smap->length);
+      phy_mem_init(smap->base, smap->length, physfree, (uint64_t)(physbase));
+      printf("physfree %x\n", (uint64_t)physfree);
 		}
 	}
+
+  physfree = physfree + (1024*1024);
+
+  set_paging((void *)&kernmem, physfree, physbase);
 	printf("tarfs in [%p:%p]\n", &_binary_tarfs_start, &_binary_tarfs_end);
-	// kernel starts here
+	read_tarfs();
 	while(1);
 }
-
-#define INITIAL_STACK_SIZE 4096
-char stack[INITIAL_STACK_SIZE];
-uint32_t* loader_stack;
-extern char kernmem, physbase;
-struct tss_t tss;
 
 void boot(void)
 {
