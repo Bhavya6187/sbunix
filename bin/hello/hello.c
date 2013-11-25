34891afda42f14fa2917edb519a6f1236e85a2fb
bhavya <bhavya.6187@gmail.com>
2013-11-25 06:47:41 -0500

    changes in rootfs

--- bin/hello/hello.c
+++ bin/hello/hello.c
@@ -1,6 +1,9 @@
-#include <stdio.h>
+# include <stdio.h>
+//# include <sys/syscall.h>
 
-int main(int argc, char* argv[]) {
-	printf("Hello World!\n");
+int main(int argc, char* argv[]) 
+{
+	sys_putint(6187);
+	while(1);
 	return 0;
 }
