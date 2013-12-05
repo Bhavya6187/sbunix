cf5a30f6c02e71d2c6f754aadff8cbe48d3febc6
Dushyant Goyal <goyal1dushyant@gmail.com>
2013-12-05 07:43:10 -0500

    test files wait and sch

---
+++ bin/test_wait/test_wait.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <syscall.h>
+#include <libc/malloc.h>
+#include <stdarg.h>
+#include <defs.h>
+
+int main(int argc, char* argv[]) 
+{
+  int ret1 = fork(); 
+  int temp =0, i,j; 
+  temp++;
+  char filename[15] = "bin/world";
+  if(ret1==0)
+  {
+    //temp = 20;
+    u_printf("fork return:%d:", ret1);
+    u_printf("This is child !! \n");
+    //__syscall1(SYSCALL_EXIT, 0);
+    //exit(0);
+    //ret1 = __syscall1(SYSCALL_EXECVE,(uint64_t)(filename));
+    execve(filename);
+    u_printf("In child return from exec()\n");
+    while(1);
+      u_printf("Hiiii\n");
+  }
+  else
+  {
+    if(ret1>1)
+    {
+      u_printf("fork return:%d:", ret1);
+      u_printf("This is parent!! \n");
+      //ret1 = __syscall0(SYSCALL_SCHEDULER);
+      yield();
+      //while(1);
+      u_printf("Returning back from schedule in parent!! \n");
+      //  u_printf("Fuck UUUUU\n");
+      yield();
+      //ret1 = __syscall0(SYSCALL_SCHEDULER);
+      while(1)
+      {
+        for(i=0;i<500;i++)
+        for(j=0;j<500;j++)
+          ;
+        //u_printf("In Parent after schedule\n");
+      }
+      //u_printf("LALALALLALALALALLALA !! ... \n");
+    }
+
+  }
+
+  //u_printf(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>\n"); 
+  
+  /*
+  struct ad *ad1 = (struct ad*) malloc(sizeof(struct ad));
+  struct ad *ad1 = (struct ad*) malloc(sizeof(struct ad));
+  ad1->t1 = 123213213;
+  ad1->t2 = 99;
+  ad1->t3 = 2324564;
+  ad1->t4 = 23244566;
+  ad1->t5 = 23244566;
+  ad1->t6 = 0;
+  ad1->t7 = 2324456;
+  ad1->t8 = 2324456;
+  u_printf("%d:%d:%d:\n%d:%d:%d\n:%d:%d", ad1->t1,ad1->t2,ad1->t3,ad1->t4,ad1->t5,ad1->t6,ad1->t7,ad1->t8);*/
+  
+  /*int tester;
+  u_scanf("%d",(uint64_t)&tester);
+  ret = u_printf("scanf gave us %d",tester);
+  
+  char test_scan[6];
+  u_scanf("%s",(uint64_t)&test_scan);
+  ret = u_printf("scanf gave us %s",test_scan);
+  */
+  //while(1);
+  /*uint64_t i=0, j=0;
+  
+  while(1)
+  {
+    for(i=0;i<500;i++)
+    for(j=0;j<500;j++)
+      ;
+    u_printf("%c", 'D');
+  }*/
+	return 0;
+}
